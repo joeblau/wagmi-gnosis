4ab3c7897606a8904f97e0debf8bfb230f084e00
Joe Blau <im@joeblau.com>
2022-06-26 13:20:29 -0700

    wagmi gnosis demo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { WagmiConfig, createClient } from "wagmi";
+
+const client = createClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiConfig client={client}>
+      <Component {...pageProps} />
+    </WagmiConfig>
+  );
 }
 
-export default MyApp
+export default MyApp;
